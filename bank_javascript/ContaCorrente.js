9b74b1a6fa72706a4c8c332e0606215441698526
FaaF-Eng <fabio.teixeira18@hotmail.com>
2024-11-28 15:33:32 -0300

    system final update

--- bank_javascript/ContaCorrente.js
+++ bank_javascript/ContaCorrente.js
@@ -1,12 +1,16 @@
 import {Cliente} from "./Cliente.js";
 
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
     _cliente;
+    _saldo =0;
 
+
+    //Assessores
     set cliente(novoValor){
-        if(novoValor instanceof Cliente)
-            this._cliente = novoValor;
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;}
     }  
 
 
@@ -14,12 +18,20 @@ export class ContaCorrente{
         return this._cliente;
     }
 
-    _saldo =0;
+
 
     get saldo(){
         return this._saldo;
     }
 
+    constructor (agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+
+    }
+
+    
     sacar(valor){ //Operação de sacar
         if(this._saldo >= valor){ 
             this._saldo -= valor;
--- bank_javascript/index.js
+++ bank_javascript/index.js
@@ -3,29 +3,17 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente(); 
-cliente1.nome = "Fábio";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Fábio",11122233309 ); 
+const cliente2 = new Cliente("Jonas",88822233309 );
 
-// const cliente2 = new Cliente();
-// cliente2.nome = "Jonas";
-// cliente2.cpf = 88822233309;
+const contaCorrenteFabio = new ContaCorrente(1001, cliente1);
 
 
-
-const contaCorrenteFabio = new ContaCorrente();
-contaCorrenteFabio.agencia= 1001;
-contaCorrenteFabio.cliente = cliente1;
 contaCorrenteFabio.depositar(500);
+const conta2 = new ContaCorrente(102, cliente2);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 102;
 
 let valor = 200;
+contaCorrenteFabio.transferir(valor, conta2);
 
-contaCorrenteFabio.transferir(valor,conta2);
-console.log(conta2.saldo);
-
-
-
+console.log(ContaCorrente.numeroDeContas);
